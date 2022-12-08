058b048561a6e5303a4fadd6801cf4f8e92a3f3e
alvaro-gc95 <alvaro@intermet.es>
2022-12-08 12:19:42 +0100

    Separate climatological calculations in a different script

---
+++ scripts/climate.py
@@ -0,0 +1,108 @@
+"""
+Calculate climatological aspects of a site
+
+Contact: alvaro@intermet.es
+"""
+
+import itertools
+
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+import utils
+
+
+class Climatology:
+    def __init__(self, pandas_obj):
+        self._obj = pandas_obj
+
+    def daily_cycle(self, percentiles=None, to_series=False):
+        """
+        Calculate the percentiles of the monthly daily cycles
+        """
+        if percentiles is None:
+            percentiles = [0.1, 0.25, 0.5, 0.75, 0.9]
+
+        columns = [v + '_' + str(p) for v, p in itertools.product(self._obj.columns, percentiles)]
+        # Calculate the index of the monthly daily cycles (format = hour_month)
+        idx = [str(h) + '_' + str(m) for h, m in itertools.product(range(0, 24), range(1, 13))]
+
+        # Dataframe of climatological percentiles
+        climatology_percentiles = pd.DataFrame(index=idx, columns=columns)
+        climatology_series = pd.DataFrame(index=self._obj.index, columns=columns)
+
+        # Calculate the climatological daily cycle for each month for each percentile
+        for variable, percentile in itertools.product(self._obj.columns, percentiles):
+            for month, month_dataset in self._obj.groupby(self._obj.index.month):
+                print(month_dataset.dtypes)
+                monthly_climatology = month_dataset[variable].groupby(month_dataset.index.hour).quantile(percentile)
+                for hour in monthly_climatology.index:
+                    climatology_percentiles.loc[str(hour) + '_' + str(month), variable + '_' + str(percentile)] = \
+                        monthly_climatology.loc[hour]
+
+        # transform the monthly daily cycles to time series
+        if to_series:
+            for variable, percentile in itertools.product(self._obj.columns, percentiles):
+                for month, month_dataset in climatology_series.groupby(climatology_series.index.month):
+                    for hour, hourly_dataset in month_dataset.groupby(month_dataset.index.hour):
+                        climatology_series.loc[hourly_dataset.index, variable + '_' + str(percentile)] = \
+                            climatology_percentiles.loc[str(hour) + '_' + str(month), variable + '_' + str(percentile)]
+            return climatology_series
+        else:
+            return climatology_percentiles
+
+    def spatial_corr(self, related_site, percentiles=None):
+        """
+        Get the correlation and linear regression with a reference station
+        """
+        if percentiles is None:
+            percentiles = [0.1, 0.25, 0.5, 0.75, 0.9]
+
+        # Calculate the index of tha monthly daily cycles (format = hour_month)
+        idx = [str(h) + '_' + str(m) for h, m in itertools.product(range(0, 24), range(1, 13))]
+        columns = [v + '_' + lr for v, lr in itertools.product(self._obj.columns, ['coef', 'intercept', 'correlation'])]
+        # Dataframe of climatological percentiles
+        regression = pd.DataFrame(index=idx, columns=columns)
+        residuals = pd.DataFrame(index=self._obj.index, columns=[c + '_residuals' for c in self._obj.columns])
+
+        # Group the data by month
+        for month, month_dataset in self._obj.groupby(self._obj.index.month):
+            # Fill the dataframe with the climatological daily cycle of each month
+            for hour, hour_dataset in month_dataset.groupby(month_dataset.index.hour):
+
+                # Select the data of the reference station by month and hour
+                related_site_hm = related_site.loc[
+                    (related_site.index.month == month) &
+                    (related_site.index.hour == hour)]
+                hour_dataset = hour_dataset
+
+                # Correlate the datasets
+                correlation = related_site_hm.corrwith(hour_dataset)
+
+                for variable in self._obj.columns:
+                    # Clean DataFrames of possible conflictive values
+                    x = utils.clean_dataset(related_site_hm[variable].to_frame())
+                    y = utils.clean_dataset(hour_dataset[variable].to_frame())
+
+                    # Get only the common data
+                    common_idx = list(set(x.index).intersection(y.index))
+                    x = x.loc[common_idx]
+                    y = y.loc[common_idx]
+
+                    # Create object for the class
+                    linear_regressor = LinearRegression()
+                    # Perform linear regression
+                    linear_regressor.fit(x, y)
+                    regr_res = x - linear_regressor.predict(x)
+
+                    # Save the coefficient, intercept, and correlation for the hour and month
+                    regression.loc[str(hour) + '_' + str(month),
+                                   variable + '_coef'] = np.squeeze(linear_regressor.coef_)
+                    regression.loc[str(hour) + '_' + str(month),
+                                   variable + '_intercept'] = np.squeeze(linear_regressor.intercept_)
+                    regression.loc[str(hour) + '_' + str(month), variable + '_correlation'] = correlation[variable]
+
+                    residuals.loc[regr_res.index, variable + '_residuals'] = regr_res[variable].values
+
+        return regression, residuals
